0612cb678d7858f41e1ccf0ebbc6b7a996e7957f
bbusn <benoit.busnardo@gmail.com>
2025-01-12 22:11:10 +0100

    base structure

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ['Le-Murmure', 'sans-serif'],
+        secondary: ['LiberationSans', 'sans-serif'],
+      },
+      colors: {
+        primary: '#333',
+        secondary: {
+          100: '#F7F7F7',
+          200: '#EDEDED',
+          300: '#D6D6D6',
+          400: '#A3A3A3',
+          500: '#4D4D4D',
+        },
+      },
+    }
+  },
+
+  plugins: []
+} satisfies Config;
